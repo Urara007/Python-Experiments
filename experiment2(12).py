ed6249e29e56e804a6073fb5433245ed29498be4
urara4ya <126614432+Urara007@users.noreply.github.com>
2026-02-26 21:58:03 +0530

    Add files via upload

---
+++ experiment2(12).py
@@ -0,0 +1,6 @@
+sequence = (10, 20, 56, 78, 89)
+num = int(input("Enter number to check: "))
+if num in sequence:
+    print(f"{num} is in the sequence")
+else:
+    print(f"{num} is NOT in the sequence")
