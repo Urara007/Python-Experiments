667fd6e754b1a7c1c3f2834600eb8e6155ddf7b6
urara4ya <126614432+Urara007@users.noreply.github.com>
2026-02-26 19:14:30 +0530

    Add files via upload

---
+++ experiment6.py
@@ -0,0 +1,16 @@
+name = "NIKUNJ BANSAL"
+sap_id = 500069944
+dob = "13 Oct 1999"
+address_line1 = "UPES"
+address_line2 = "Bidholi Campus"
+pincode = 248007
+programme = "AI & ML"
+semester = 2
+print(f"NAME: {name}")
+print(f"SAP ID: {sap_id}")
+print(f"DATE OF BIRTH: {dob}")
+print(f"ADDRESS: {address_line1}")
+print(f"         {address_line2}")
+print(f"         Pincode: {pincode}")
+print(f"Programme: {programme}")
+print(f"Semester: {semester}")
