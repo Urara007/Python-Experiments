103e4fb95195328314906a0b6bc6d468337dfe84
urara4ya <126614432+Urara007@users.noreply.github.com>
2026-02-26 21:36:35 +0530

    Add files via upload

---
+++ experiment2(5).py
@@ -0,0 +1,5 @@
+p = float(input("Enter Principal: "))
+r = float(input("Enter Rate of Interest: "))
+t = float(input("Enter Time (years): "))
+si = (p * r * t) / 100
+print(f"Simple Interest: {si}")
