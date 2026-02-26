b8362896f37a96f05b0a1276c064609d9bf46b6b
urara4ya <126614432+Urara007@users.noreply.github.com>
2026-02-26 21:43:26 +0530

    Add files via upload

---
+++ experiment2(11).py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+shift = int(input("Enter shift value: "))
+print(f"Left Shift ({num} << {shift}): {num << shift}")
+print(f"Right Shift ({num} >> {shift}): {num >> shift}")
